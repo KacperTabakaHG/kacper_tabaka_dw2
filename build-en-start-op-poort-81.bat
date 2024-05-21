12901e6a54a331c5839a98c6652432afa157e656
KacperTabakaHG <kacper.tabaka@student.hogent.be>
2024-05-21 16:20:41 +0200

    ggr

--- build-en-start-op-poort-81.bat
+++ build-en-start-op-poort-81.bat
@@ -1,2 +1,2 @@
 docker image build -t mydocker:22.04 .
-docker run -d -t --name mydocker -p kacpertabaka.northeurope.cloudapp.azure.com:81:80 mydocker:22.04
+docker run -d -t --name mydocker -p 13.74.162.243:81:80 mydocker:22.04
